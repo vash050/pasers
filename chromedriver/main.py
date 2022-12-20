71f8229ac71587c5d54ddc18fe90262da31751df
vasiliy <vash050@rambler.ru>
2022-12-20 18:06:57 +0300

    full screen

--- chromedriver/main.py
+++ chromedriver/main.py
@@ -6,6 +6,9 @@ from selenium.webdriver.common.by import By
 
 options = webdriver.ChromeOptions()
 options.add_argument(f'user-agent= {UserAgent().random}')
+options.add_experimental_option("detach", True)
+options.add_argument("--start-maximized")
+options.add_argument('window-size=2560,1440')
 
 driver = webdriver.Chrome(
     executable_path='/home/vasiliy/my_prodject/parsers/chromedriver/chromedriver',
@@ -13,18 +16,30 @@ driver = webdriver.Chrome(
 )
 
 url = 'https://moidom.citylink.pro/pub/212'
+
+
 # url = 'https://www.whatismybrowser.com/detect/what-is-my-user-agent'
 
 
 def main():
     try:
         driver.get(url=url)
+        time.sleep(2)
 
         button_save_late = driver.find_element(By.CLASS_NAME, "btn-save-later")
-        print(button_save_late.text)
         button_save_late.click()
 
-        time.sleep(10)
+        play = driver.find_element(By.CLASS_NAME, 'vjs-big-play-button')
+
+        play.click()
+
+        full_screen = driver.find_element(By.CLASS_NAME, "vjs-fullscreen-control")
+        full_screen.click()
+
+
+
+        time.sleep(360000)
+
     except Exception as e:
         print(e)
     finally:
